4d5ec3a21da27ed9648d4d218f8de5e14faf0bee
iwmvictor <isingizwemunezerovictor5@gmail.com>
2025-11-25 17:00:09 +0200

    keywords

--- src/mock/company.js
+++ src/mock/company.js
@@ -3,12 +3,12 @@ import { assets } from "./asset";
 export const companyInfo = {
   phoneMtn: "250789829879",
   phoneAirtel: "250735666013",
-  gmail: "brightsafarisafrica@gmail.com",
-  //   email: "info@brightsafaris.com",
+  // gmail: "brightsafarisafrica@gmail.com",
+  email: "info@brightsafarisafrica.com",
   location: "Kigali, Rwanda",
   address: {
     title: "Kigali, Rwanda",
-    road: "KG 512 St.",
+    road: "KG 237 St",
     mapLink: "",
     map: "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d249.21903464123113!2d30.115253570944226!3d-1.9513797128724537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sPharmacies!5e0!3m2!1sen!2srw!4v1763955829816!5m2!1sen!2srw",
     name: "Bright Safaris Africa - Near Contrôle Technique",
